eef733e34b58214b84c9cd5cc7cce06244a950cd
evezen <piroko235@gmail.com>
2024-09-13 18:03:24 -0300

    formulario e tabela usando javascript

--- pages/aula05/part02/se.js
+++ pages/aula05/part02/se.js
@@ -1,6 +1,6 @@
-function igual () {
+function igual() {
     var x = 2
-    var y = 2 
+    var y = 2
     let res
     if (x == y) {
         res = "Iguais"
@@ -9,5 +9,46 @@ function igual () {
     }
     document.getElementById("resultado-igual").innerHTML = res
     document.getElementById("confirmado-igual").innerHTML = "Resultado ✅"
-    document.getElementById("confirmado-igual").style.backgroundColor="green"
+    document.getElementById("confirmado-igual").style.backgroundColor = "green"
+}
+
+function diferente() {
+    var x = 2
+    var y = 3
+    let res
+    if (x != y) {
+        res = "Diferentes"
+    } else {
+        res = "Iguais"
+    }
+    document.getElementById("resultado-diferente").innerHTML = res
+    document.getElementById("confirmado-diferente").innerHTML = "Resultado ✅"
+    document.getElementById("confirmado-diferente").style.backgroundColor = "green"
+}
+
+function maior() {
+    var x = 4
+    var y = 2
+    let res
+    if (x > y) {
+        res = "Maior"
+    } else {
+        res = "Menor ou Igual"
+    }
+    document.getElementById("resultado-maior").innerHTML = res
+    document.getElementById("confirmado-maior").innerHTML = "Resultado ✅"
+    document.getElementById("confirmado-maior").style.backgroundColor = "green"
+}
+
+function menor() {
+    var x = 2
+    var y = 2
+    if (x < y) {
+        res = "Menor"
+    } else {
+        res = "Maior ou Igual"
+    }
+    document.getElementById("resultado-menor").innerHTML = res
+    document.getElementById("confirmado-menor").innerHTML = "Resultado ✅"
+    document.getElementById("confirmado-menor").style.backgroundColor = "green"
 }
