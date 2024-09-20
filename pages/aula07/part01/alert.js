b3b95234ad6ceba1f5a98c59c5c83ffd897f8690
evezen <piroko235@gmail.com>
2024-09-20 16:54:07 -0300

    introdução a alert, confirm e prompt

---
+++ pages/aula07/part01/alert.js
@@ -0,0 +1,4 @@
+window.alert("Minha primeira mensagem!");
+window.confirm("Está gostando do curso?");
+let res = window.prompt("Qual o seu nome?");
+document.getElementsByTagName("p").innerhtml = res;
